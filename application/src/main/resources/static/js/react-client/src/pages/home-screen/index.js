1e4e03355e4094eb9be967838450b7a2b074d326
crlanier22 <crlanier22@gmail.com>
2023-01-11 22:57:38 -0800

    created logic and requests for 'Perishables' in the homescreen

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
+import React from 'react';
+import Axios from "axios";
+import style from './style.module.scss';
 
 const HomeScreen = () => {
+  const [loadingPerishables, setLoadingPerishables] = useState(true);
+  const [perishables, setperishables] = useState(null);
+
+  useEffect(() => {
+
+
+    const fetchData = async () => {
+      const result = await Axios(`http://localhost:8080/perishables`);
+      setperishables(result.data);
+    }
+
+    if (perishables) {
+      setLoadingPerishables(false)
+    }
+
+    !perishables && fetchData();
+
+  }, [perishables]);
+
+
+
   return (
-    <h1>This is the home screen</h1>
+    <div>
+      {loadingPerishables ? <h3>Loading...</h3> :
+        <div>
+          <h2>Perishable Items</h2>
+          <ul className={style.itemsContainer}>
+            {perishables.map(perishableItem => (
+              <div key={perishableItem.id}>
+                <img src={perishableItem.imageUrl} alt='' />
+                <h3>{perishableItem.name}</h3>
+                <p>Quantity: {perishableItem.quantity}</p>
+                <p>Price: {perishableItem.price}</p>
+                <p>Days to Expire: {perishableItem.daysToExpiration}</p>
+              </div>
+
+            ))}
+          </ul>
+        </div>
+      }
+
+    </div>
   )
 }
 
